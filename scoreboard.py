5698e2e21752250e1f8b0f8a9b1fc8f225a8c75b
David <davidtucu@hotmail.com>
2021-04-28 08:16:14 -0300

    Add: Game High score , and reset the game.

--- scoreboard.py
+++ scoreboard.py
@@ -6,6 +6,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.hideturtle()
         self.goto(0, 265)
@@ -13,15 +14,17 @@ class Scoreboard(Turtle):
         self.update_scroeboard()
 
     def update_scroeboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: ´{self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"Game Over: {self.score}", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scroeboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scroeboard()
 
 
