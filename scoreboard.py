9c20bb28062ecdd49bfba95a579500e493f66a1c
David <davidtucu@hotmail.com>
2021-04-28 08:45:44 -0300

    Add: save the Game High score , and them load.

--- scoreboard.py
+++ scoreboard.py
@@ -1,30 +1,33 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Arial", 24, "normal")
+
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.hideturtle()
         self.goto(0, 265)
         self.color("white")
-        self.update_scroeboard()
+        self.update_scoreboard()
 
-    def update_scroeboard(self):
+    def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: ´{self.high_score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
-        self.update_scroeboard()
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.update_scroeboard()
-
-
+        self.update_scoreboard()
